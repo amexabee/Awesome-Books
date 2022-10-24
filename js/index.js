3db0d573614e8f6bc56c7b6a6126ce948e7ea229
ThankGod Richard <thankgod@lemonade.finance>
2022-10-24 21:12:07 +0100

    Clear on remove

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
-class Book{
-    constructor(name, author){
-        this.name = name;
-        this.author = author;
-    }
-}
+let bookListView = document.getElementById("booklist");
+
 
 
 let bookList =  JSON.parse(localStorage.getItem("books"));
@@ -11,28 +7,36 @@ if(bookList == null ){
     bookList = []
 }
 
-function addNewBook(book){
-    bookList.push(book)
-    localStorage.setItem("books", JSON.stringify(bookList))
+function initiew(){
+    bookListView.innerHTML = "";
+    bookList.forEach(element => {
+        const liNode =  document.createElement("li");
+        const pNode = document.createElement("p");
+        const bookKText = document.createTextNode(element.name)
+        pNode.appendChild(bookKText)
+        const buttonNode = document.createElement("button");
+        const removeKText = document.createTextNode("Remove")
+        buttonNode.appendChild(removeKText)
+    
+        buttonNode.addEventListener("click", (event) => {
+            removeBook(element)
+        })
+    
+        liNode.appendChild(pNode)
+        liNode.appendChild(buttonNode)
+        bookListView.appendChild(liNode)
+    });
+    
 }
 
+window.addEventListener('load', function () {
+     initiew()
+  }, false);
+
 function removeBook(book){
-  bookList =  bookList.filter((singleBook) => {
-       return (book.name == singleBook.name && book.author == singleBook.author)
-    })
-    localStorage.setItem("books", JSON.stringify(bookList))
+    bookList =  bookList.filter((singleBook) => {
+         return (book.name != singleBook.name && book.author != singleBook.author)
+      })
+      localStorage.setItem("books", JSON.stringify(bookList))
+      initiew()
 }   
-
-document.getElementById("btn").addEventListener("click", (event)  => {
-    event.preventDefault();
-    let bookanme = document.getElementById("title").value;
-    let authod = document.getElementById("author").value;
-    let newBook = new Book(bookanme, author);
-    addNewBook(newBook);
-    console.log(bookList.length);
-});
-
-document.getElementById("booklist").innerHTML = 
-    bookList.forEach(element => {
-       
-    });
