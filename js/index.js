80045b3485744c3222fe99c12156c1902b7d6b59
ThankGod Richard <thankgod@lemonade.finance>
2022-10-24 18:59:39 +0100

    added js functions

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
-    
+class Book{
+    constructor(name, author){
+        this.name = name;
+        this.author = author;
+    }
+}
+
+
+let bookList =  JSON.parse(localStorage.getItem("books"));
+if(bookList == null ){
+    bookList = []
+}
+
+function addNewBook(book){
+    bookList.push(book)
+    localStorage.setItem("books", JSON.stringify(bookList))
+}
+
+function removeBook(book){
+  bookList =  bookList.filter((singleBook) => {
+       return (book.name == singleBook.name && book.author == singleBook.author)
+    })
+    localStorage.setItem("books", JSON.stringify(bookList))
+}   
+
+document.getElementById("btn").addEventListener("click", (event)  => {
+    event.preventDefault();
+    let bookanme = document.getElementById("title").value;
+    let authod = document.getElementById("author").value;
+    let newBook = new Book(bookanme, author);
+    addNewBook(newBook);
+    console.log(bookList.length);
+});
+
+document.getElementById("booklist").innerHTML = 
+    bookList.forEach(element => {
+       
+    });
