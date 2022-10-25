63b20590fef6739dd19e40170ae42148e23a5015
ThankGod Richard <thankgod@lemonade.finance>
2022-10-25 21:11:35 +0100

    updated

--- js/addnew.js
+++ js/addnew.js
@@ -6,25 +6,11 @@ class Book {
   }
 }
 
-class BookListManger {
-  constructor() {
-    this.bookList = JSON.parse(localStorage.getItem('books'));
-    if (this.bookList == null) {
-      this.bookList = [];
-    }
-  }
-
-  addNewBook(book) {
-    this.bookList.push(book);
-    localStorage.setItem('books', JSON.stringify(this.bookList));
-  }
-}
-
 document.getElementById('btn').addEventListener('click', (event) => {
   event.preventDefault();
   const bookanme = document.getElementById('title').value;
   const author = document.getElementById('author').value;
   const newBook = new Book(bookanme, author, new Date().getTime());
-  new BookListManger().addNewBook(newBook);
+  new BookListManger().addNewBook(newBook)
   document.location.href = './index.html';
 });
